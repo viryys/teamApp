92a01e2bd21399c9528efe87b5f6d02ddf92154c
Vadim Tatarchuk <tatarchuk.v.b@gmail.com>
2020-11-06 00:53:43 +0500

    Initial Commit

---
+++ teamApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  teamApp
+//
+//  Created by  Vadim Tatarchuk on 06.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
