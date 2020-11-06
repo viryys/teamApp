ca9049255a4c68584fc4c4e64d4f9da790257d0b
Vadim Tatarchuk <tatarchuk.v.b@gmail.com>
2020-11-06 19:37:52 +0500

    added new tabBar Item

---
+++ teamApp/Controllers/QuizViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuizViewController.swift
+//  teamApp
+//
+//  Created by  Vadim Tatarchuk on 06.11.2020.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
